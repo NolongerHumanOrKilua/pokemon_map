61aebd29928034050aec6c7f4ddc4ec9f2cb36b0
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-10-31 23:11:40 +0300

    Add relation to db models.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,16 +1,15 @@
-from email.policy import default
-from pickle import TRUE
 from django.db import models  # noqa F401
 
 # your models here
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    pokemon_image = models.ImageField(null=True)
+    pokemon_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return format(self.title)
 
 class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     Lat = models.FloatField(blank=True)
     Lon = models.FloatField(blank=True)
