b2cf3d9a8af48195c76b282cc09c5fc42c8c819c
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-11-02 18:44:05 +0300

    show pokemons on mainpage.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,10 +4,10 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    pokemon_image = models.ImageField(null=True, blank=True)
+    pokemon_image = models.ImageField(upload_to="media", null=True, blank=True)
 
     def __str__(self):
-        return format(self.title)
+        return self.title
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
