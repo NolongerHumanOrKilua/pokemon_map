9ecd54a7b4e3b7b2c02c95c738a5bc268171dbca
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-11-02 22:53:37 +0300

    show pokemons on mainpage map.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,7 +4,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    pokemon_image = models.ImageField(upload_to="media", null=True, blank=True)
+    pokemon_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -27,16 +27,13 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
+    for pokemon in Pokemon.objects.all():
+        for pokemon_entity in PokemonEntity.objects.filter(pokemon=pokemon):
             add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
+                folium_map, pokemon_entity.Lat,
+                pokemon_entity.Lon,
+                "http://127.0.0.1:8000" + pokemon.pokemon_image.url
             )
 
     pokemons_on_page = []
