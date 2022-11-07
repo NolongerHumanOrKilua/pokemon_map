52e8b4c9984473b0502ec862a5565eece0c5a8ee
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-11-07 17:34:19 +0300

    <что полезного сделано>

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -64,6 +64,7 @@ def show_pokemon(request, pokemon_id):
         'pokemon_id': requested_pokemon.id,
         'title_ru': requested_pokemon.title,
         'img_url': requested_pokemon.pokemon_image.url,
+        "description": requested_pokemon.description
         }
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in PokemonEntity.objects.filter(pokemon=requested_pokemon):
